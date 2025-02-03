a7df32d8320e10fbf2cde4c05e5f8376c97c2d7a
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2025-02-02 20:03:31 -0600

    Add header hamburger menu, and carousel with sliding images

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import "./styles.css";
-console.log("Testing");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-item").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+);
+
+const carouselButton = document.querySelectorAll(".carousel-button");
+carouselButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const slides = document.querySelectorAll(".slide");
+    let activeIndex = Array.from(slides).findIndex((item) =>
+      item.classList.contains("active")
+    );
+
+    slides[activeIndex].classList.remove("active");
+    let nextIndex = (activeIndex + 1) % slides.length;
+
+    slides[nextIndex].classList.add("active");
+  });
+});
